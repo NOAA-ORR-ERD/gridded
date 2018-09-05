c350498fd414633cb409b632157884c77e522814
jay.hennen <jay.hennen@noaa.gov>
2018-09-05 09:00:55 -0700

    version up

--- gridded/__init__.py
+++ gridded/__init__.py
@@ -2,7 +2,7 @@
 
 from __future__ import (absolute_import, division, print_function, unicode_literals)
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 from gridded.gridded import Dataset
 from gridded.grids import Grid
