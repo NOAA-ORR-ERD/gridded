535e580be3b2c6a5875064ed983251c0c056dccf
Jay Hennen <jay.hennen@noaa.gov>
2024-01-11 16:59:43 -0800

    version to 0.6.0

--- gridded/__init__.py
+++ gridded/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-__version__ = "0.5.9"
+__version__ = "0.6.0"
 
 VALID_SGRID_LOCATIONS = (None, 'center', 'edge1', 'edge2', 'node')
 VALID_UGRID_LOCATIONS = (None, 'node', 'face', 'edge', 'boundary')
