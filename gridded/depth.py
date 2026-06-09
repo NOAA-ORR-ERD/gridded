f3055d8c61ff0b4d5a93fc8607bf7850ab9c9f9a
Jay Hennen <jay.hennen@noaa.gov>
2026-06-09 09:27:15 -0700

    depth get_transect zeta compensation

--- gridded/depth.py
+++ gridded/depth.py
@@ -839,8 +839,8 @@ class ROMS_Depth(S_Depth):
     @property
     def num_layers(self):
         return len(self.s_rho)
-    
-    def get_s_coord(self, points, time, data_shape=None, _hash=None, **kwargs):
+
+    def get_s_coordinate(self, points, time, data_shape=None, _hash=None, **kwargs):
         """
         :param points: array of points to interpolate to
         :type points: numpy array of shape (n, 3)
@@ -860,8 +860,8 @@ class ROMS_Depth(S_Depth):
 
         s_c = self.s_rho if data_shape[0] == self.num_layers else self.s_w
         C_s = self.Cs_r if data_shape[0] == self.num_layers else self.Cs_w
-        h = self.bathymetry.at(points, time, unmask=False, _hash=_hash, **kwargs)
-        zeta = self.zeta.at(points, time, unmask=False, _hash=_hash, **kwargs)
+        h = self.bathymetry.at(points, time, _hash=_hash, **kwargs)
+        zeta = self.zeta.at(points, time, _hash=_hash, **kwargs)
         hc = self.hc
         hCs = h * C_s[np.newaxis, :]
         if self.vtransform == 1:
@@ -871,6 +871,10 @@ class ROMS_Depth(S_Depth):
             S = ((hc * s_c) + hCs) / (hc + h)
             s_coord = -(zeta + (zeta + h) * S)
         return s_coord
+    
+    def get_transect(self, points, time, data_shape=None, _hash=None, **kwargs):
+        zeta = self.zeta.at(points, time, _hash=_hash, **kwargs)
+        return self.get_s_coordinate(points, time, data_shape=data_shape, _hash=_hash, **kwargs) + zeta
 
 
 class FVCOM_Depth(S_Depth):
